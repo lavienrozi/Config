c232289412912067fe0f8c1ff4a60ceaf0dbb43b
lavienrozi <104704814+lavienrozi@users.noreply.github.com>
2022-09-17 21:33:07 -0500

    Delete Nighty.js
    Delete Nighty.js from config

--- Nighty.js
+++
@@ -1,4 +0,0 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#f2777a", "#99cc99", "#ffcc66", "#6699cc", "#cc99cc", "#66cccc", "#ffffff", "#000000", "#f2777a", "#99cc99", "#ffcc66", "#6699cc", "#cc99cc", "#66cccc", "#ffffff"]);
-t.prefs_.set('foreground-color', "#e5cea9");
-t.prefs_.set('background-color', "#2d2d2d");
-t.prefs_.set('cursor-color', 'rgba(204,204,204,0.5)');
